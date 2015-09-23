42978b6c7e4959296bdd622fc3dc4429c5143482
Irina Potapova <inventeddesign@gmail.com>
2015-09-23 16:26:39 +0300

    варзар7

--- VARZAR/js/script.js
+++ VARZAR/js/script.js
@@ -10,7 +10,6 @@ jQuery(document).ready(function($){
   });
 });
 
-
 jQuery(document).ready(function($){
   $(window).scroll(function(event){
     var pix = $(window).scrollTop();
@@ -41,131 +40,47 @@ jQuery(document).ready(function($){
   });
 });
 
-//
-// $( document ).on('mouseenter', '[class*="smallicon"], [class*="describe"], [class*="heading3"]', function( event ) {
-//   var $this = $(event.target);
-//   var ThisClass = $this.attr('class');
-//   var coloring = ThisClass.substring(0,3);
-//   var i = ThisClass.slice(-1);
-//   if (coloring == "ora") {
-//     $(".orange__heading3-"+i).css({'font-weight' : 'bold'});
-//   }
-//   else if (coloring == "blu") {
-//     alert("blue");
-//   }
-//   else {
-//     alert("red");
-//   }
-//
-//
-//   var icon = ThisClass.substring(8,17);
-//   var descr =  ThisClass.substring(8,16);
-//   var heading =  ThisClass.substring(8,16);
-// });
-//
-// $( document ).on('mouseover', '[class*="smallicon"], [class*="describe"], [class*="heading3"]', function( event ) {
-//   var $this = $(event.target);
-//   var ThisClass = $this.attr('class');
-//   var coloring = ThisClass.substring(0,3);
-//   var i = ThisClass.slice(-1);
-//   if (coloring == "ora") {
-//     $(".orange__heading3-"+i).css({'font-weight' : 'bold'});
-//   }
-//   else if (coloring == "blu") {
-//     alert("blue");
-//   }
-//   else {
-//     alert("red");
-//   }
-//
-//
-//   var icon = ThisClass.substring(8,17);
-//   var descr =  ThisClass.substring(8,16);
-//   var heading =  ThisClass.substring(8,16);
-// });
-//
-//
-//
-//
 
+$( document ).on('mouseenter', '[class*="smallicon"], [class*="describe"], [class*="heading3"]', function( event ) {
+  var $this = $(event.target);
+  var ThisClass = $this.attr('class');
+  var coloring = ThisClass.substring(0,3);
+  var i = ThisClass.slice(-1);
 
+  if (coloring == "ora") {
+    $(".orange__heading3-" + i).css({'font-weight' : 'bold'})
+    $(".orange__describe-" + i).css({'background-color' : '#f9e1cc'})
+    $(".orange__smallicon-" + i).addClass("orange__smallicon-hover" + i);
+  }
+  else if (coloring == "blu") {
+    $(".blue__heading3-" + i).css({'font-weight' : 'bold'})
+    $(".blue__describe-" + i).css({'background-color' : '#bee4e3'})
+    $(".blue__smallicon-" + i).addClass("blue__smallicon-hover" + i);
+  }
+  else {
+    $(".red__heading3-" + i).css({'font-weight' : 'bold'})
+    $(".red__describe-" + i).css({'background-color' : '#fccdc2'})
+    $(".red__smallicon-" + i).addClass("red__smallicon-hover" + i);  }
+});
 
+$( document ).on('mouseout', '[class*="smallicon"], [class*="describe"], [class*="heading3"]', function( event ) {
+  var $this = $(event.target);
+  var ThisClass = $this.attr('class');
+  var coloring = ThisClass.substring(0,3);
+  var i = ThisClass.slice(-1);
 
-
-
-// ЧТО_ТО ОЧЕНЬ УМНОЕ, РАЗОБРАТЬСЯ
-// $( document ).on('mouseenter', '[class*="smallicon"], [class*="describe"], [class*="heading3"]', function( event ) {
-//   var $this = $(event.target);
-//   return AllHovers( $this.attr('class') );
-// });
-//
-//
-//
-// jQuery(document).ready(function AllHovers(a){
-//   alert(a);
-// });
-
-
-
-
-// ИРА МОЛОДЕЦ, но ход мыслей неверный
-// jQuery(document).ready(function($){
-//   $("*").hover(function(event){
-//   var n = $(this).attr("class");
-//   var icon = n.substring(8,17);
-//   var descr =  n.substring(8,16);
-//   var heading =  n.substring(8,16);
-//   // alert(icon + " " + descr + " " + heading + " " + n);
-//   if (icon == "smallicon" || descr == "describe" || heading == "heading3") {
-//     alert("cool!" + " " + n);
-//   }
-//   });
-// });
-
-
-
-
-
-
-
-// НЕ РАБОТАЕТ
-// jQuery(document).ready(function($){
-//   if (($("div.orange__smallicon-1").hover) || ($("div.orange__list-1").hover) || ($("div.orange__describe-1").hover)){
-//     (function(event){
-//       $(".orange__heading3-1").css({'font-weight' : 'bold'});
-//       $(".orange__smallicon-1").addClass("orange__smallicon-1hover");
-//       $("div.orange__list-1").css({'background-color' : 'red'});
-//     }, (function(event){
-//       $(".orange__heading3-1").css({'font-weight' : 'normal'});
-//       $(".orange__smallicon-1").addClass("orange__smallicon-1hover");
-//       $("div.orange__list-1").css({'background-color' : 'green'});
-//   });
-// });
-
-
-
-
-
-
-
-
-// РАБОТАЮТ ДЛЯ ЧАСТНОГО СЛУЧАЯ
-
-
-// jQuery(document).ready(function($){
-//   $("div.orange__smallicon-1").hover(function(event){
-//     $(".orange__heading3-1").css({'font-weight' : 'bold'});
-//   }, function(){
-//     $(".orange__heading3-1").css({'font-weight' : 'normal'});
-//   });
-// });
-//
-//
-//
-// jQuery(document).ready(function($){
-//   $(".orange__describe-1").hover(function(event){
-//     $(".orange__smallicon-1").addClass("orange__smallicon-1hover");
-//   }, function(){
-//     $(".orange__smallicon-1").removeClass("orange__smallicon-1hover");
-//   });
-// });
+  if (coloring == "ora") {
+    $(".orange__heading3-" + i).css({'font-weight' : 'normal'})
+    $(".orange__describe-" + i).css({'background-color' : 'transparent'})
+    $(".orange__smallicon-" + i).removeClass("orange__smallicon-hover" + i);
+  }
+  else if (coloring == "blu") {
+    $(".blue__heading3-" + i).css({'font-weight' : 'normal'})
+    $(".blue__describe-" + i).css({'background-color' : 'transparent'})
+    $(".blue__smallicon-" + i).removeClass("blue__smallicon-hover" + i);
+  }
+  else {
+    $(".red__heading3-" + i).css({'font-weight' : 'normal'})
+    $(".red__describe-" + i).css({'background-color' : 'transparent'})
+    $(".red__smallicon-" + i).removeClass("red__smallicon-hover" + i);  }
+});
